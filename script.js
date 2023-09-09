1f0d266e6472339fc37cac6961fd1334cc1d11c9
Tanmayjain0906 <tanmayjain983@gmail.com>
2023-09-10 00:11:11 +0530

    complete

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let input = document.getElementById("input-box");
+let topInput = document.getElementById("top-input-box");
+let bottom = document.getElementById("bottom-input-box");
 
 let str = "";
 
@@ -7,6 +9,8 @@ let buttons = document.querySelectorAll("button");
 let arr = Array.from(buttons);
 let dotCount = 0;
 
+let startResult = false;
+
 arr.forEach((button) => {
     button.addEventListener("click", (e) => {
 
@@ -14,26 +18,32 @@ arr.forEach((button) => {
             if (dotCount > 0) {
                 let ans = eval(str);
                 input.value = ans.toFixed(2);
-               
+
                 dotCount = 0;
             }
             else {
                 let ans = eval(str);
                 input.value = ans;
-                
-            }
 
+            }
+            startResult = false;
+            bottom.value = "";
+            topInput.value = str;
         }
 
         else if (e.target.innerText == 'AC') {
             str = "";
             input.value = str;
+            topInput.value = "";
+            bottom.value = str;
+            
         }
 
         else if (e.target.innerText == 'DEL') {
             let remainString = str.substring(0, str.length - 1);
             input.value = remainString;
             str = remainString;
+            topInput.value = "";
         }
 
         else if (e.target.innerText == '%' || e.target.innerText == '/' || e.target.innerText == '*' || e.target.innerText == '-' || e.target.innerText == '+') {
@@ -42,6 +52,8 @@ arr.forEach((button) => {
             if (check != '%' && check != '/' && check != '*' && check != '-' && check != '+') {
                 str += e.target.innerText;
                 input.value = str;
+                startResult = true;
+                topInput.value = "";
             }
         }
 
@@ -51,6 +63,20 @@ arr.forEach((button) => {
             }
             str += e.target.innerText;
             input.value = str;
+        }
+
+        if (startResult == true) {
+
+            
+            let evaluate = eval(str);
+            if (evaluate != undefined) {
+                bottom.value = evaluate;
+            }
+
+            else {
+                bottom.value = "";
+            }
+
         }
 
     })
